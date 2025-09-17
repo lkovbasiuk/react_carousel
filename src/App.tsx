338d35cf44812be9094e1eeebbd4ecde484845ac
Liudmyla Kovbasiuk <lchubatiuk96@gmail.com>
2025-09-17 15:17:17 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -20,17 +20,77 @@ class App extends React.Component<{}, State> {
       './img/9.png',
       './img/10.png',
     ],
+    step: 3,
+    frameSize: 3,
+    itemWidth: 130,
+    animationDuration: 1000,
+    infinite: false,
   };
 
   render() {
-    const { images } = this.state;
+    const { images, step, frameSize, itemWidth, animationDuration, infinite } =
+      this.state;
 
     return (
       <div className="App">
         {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+        <h1 data-cy="title">Carousel with {images.length} images</h1>
 
-        <Carousel />
+        <label>
+          Step:
+          <input
+            type="number"
+            value={step}
+            onChange={e => this.setState({ step: +e.target.value })}
+          />
+        </label>
+
+        <label>
+          Frame Size:
+          <input
+            type="number"
+            value={frameSize}
+            onChange={e => this.setState({ frameSize: +e.target.value })}
+          />
+        </label>
+
+        <label>
+          Item Width:
+          <input
+            type="number"
+            value={itemWidth}
+            onChange={e => this.setState({ itemWidth: +e.target.value })}
+          />
+        </label>
+
+        <label>
+          Animation Duration:
+          <input
+            type="number"
+            value={animationDuration}
+            onChange={e =>
+              this.setState({ animationDuration: +e.target.value })
+            }
+          />
+        </label>
+
+        <label>
+          Infinite:
+          <input
+            type="checkbox"
+            checked={infinite}
+            onChange={e => this.setState({ infinite: e.target.checked })}
+          />
+        </label>
+
+        <Carousel
+          images={images}
+          step={step}
+          frameSize={frameSize}
+          itemWidth={itemWidth}
+          animationDuration={animationDuration}
+          infinite={infinite}
+        />
       </div>
     );
   }
